c536802e41bafe1378e7e16c528f0f6b02126150
SugiKen <ynmk1995@gmail.com>
2018-04-09 21:49:28 +0900

    [add]meta

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'slim-rails'
 gem 'materialize-sass'
 
 gem 'dotenv-rails'
+gem 'meta-tags'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,32 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '就活武勇伝',
+      separator: '',
+      title: '就活武勇伝',
+      description: '【匿名】あなたの就活武勇伝を聞かせてください！',
+      keywords: '就活',
+      canonical: 'http://buyuden.popapp.me/',
+      og: {
+        title: :title,
+        type: 'website',
+        url: 'http://www.rep-rikkyo.com/',
+        image: image_url("screen.png"),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        title: :title,
+        site: '@BuyudenShukatsu',
+        card: 'summary',
+        description: :description,
+        image: image_url("screen.png"),
+        url: :url
+      }
+    }
+  end
+
   def hbr(str)
     str = html_escape(str)
     str.gsub(/\r\n|\r|\n|¥n/, "<br />").html_safe
